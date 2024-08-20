dcfed54bcb9f2ce16ae76ca2627aa8ab996ac3cd
huss2012 <huss24940@gmail.com>
2024-08-20 07:11:58 +0300

    Phase join Table Association is done.

---
+++ server/db/migrations/20240820035615-create-insect-tree.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('InsectTrees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      insectId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'cascade'
+      },
+      treeId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'cascade'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        default: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        default: Sequelize.literal("CURRENT_TIMESTAMP")
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('InsectTrees');
+  }
+};
---
+++ server/db/models/insecttree.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class InsectTree extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  InsectTree.init({
+    insectId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      onDelete: 'cascade'
+    },
+    treeId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      onDelete: 'cascade'
+    }
+  }, {
+    sequelize,
+    modelName: 'InsectTree',
+  });
+  return InsectTree;
+};
